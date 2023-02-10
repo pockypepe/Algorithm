1c20916e8dc0bde25cd9fcaa07a80bd51f19b633
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-02-10 17:25:52 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.41 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,30 +1,11 @@
-public class Solution {
+class Solution {
     public int solution(String s) {
-        int answer = 0;
-        
-		StringBuilder sb = new StringBuilder();
-		String[] input = s.replaceAll("\"", "").split("");
-		String[] number = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-		String[] str = new String[] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-		StringBuilder sb2 = new StringBuilder();
-		for (int i = 0; i < input.length; i++) {
-			int cnt = 0;
-			for (int j = 0; j < number.length; j++) {
-				if (input[i].equals(number[j])) {
-					sb.append(input[i]);
-					break;
-				}
-				cnt++;
-				if (cnt == 10) sb2.append(input[i]);
-			}
-			for (int j = 0; j < str.length; j++) {
-				if (str[j].equals(sb2.toString())) {
-					sb.append(number[j]);
-					sb2.setLength(0);
-				}
-			}
-		}
-        answer = Integer.parseInt(sb.toString());
-        return answer;
+    	String[] str = new String[] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    	
+    	for (int i = 0; i < str.length; i++) {
+    		s = s.replaceAll(str[i], String.valueOf(i));
+    	}
+    	
+    	return Integer.parseInt(s);
     }
 }
