5e5a15e5fcc688ff00dc858392a20f67d7ae545c
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-02-13 12:58:22 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 212 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		System.out.println((int) Math.pow(10, 9));
+	}
+}
