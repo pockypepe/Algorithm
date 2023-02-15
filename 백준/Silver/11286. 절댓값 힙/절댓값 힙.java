04555f42d2c6a8cf3b122c6430297fc6fa748e5f
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-02-15 13:39:47 +0900

    [Silver I] Title: 절댓값 힙, Time: 364 ms, Memory: 26608 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+			if (Math.abs(o1) < Math.abs(o2)) return -1;
+			else if (Math.abs(o1) > Math.abs(o2)) return 1;
+			else {
+				if (o1 < o2) return -1;
+				else return 1;
+			}
+		});
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] input = new int[n];
+		
+		for (int i = 0; i < n; i++) input[i] = Integer.parseInt(br.readLine());
+		
+		int idx = 0;
+		while (idx < n) {
+			if (input[idx] != 0) pq.offer(input[idx]);
+			else {
+				if (!pq.isEmpty()) sb.append(pq.poll()).append("\n");
+				else sb.append(0).append("\n");
+			}
+			idx++;
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
