88bc35b49342aab6c889a70cbe5652b532447393
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-02-24 14:01:39 +0900

    [Gold V] Title: 치킨 배달, Time: 224 ms, Memory: 18248 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,54 +1,59 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, ans;
-	static int[] temp;
-	static int[][] map;
-	static List<int[]> home, chicken;
+	static int n, m, min, ans;
+	static int[][] temp;
+	static List<int[]> chicken, home;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		home = new ArrayList<int[]>();
-		chicken = new ArrayList<int[]>();
-		temp = new int[M];
-		map = new int[N][N];
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		int[][] map = new int[n + 1][n + 1];
+		// 1과 2 위치 따로 저장
+		home = new ArrayList<>();
+		chicken = new ArrayList<>();
+			
 		ans = Integer.MAX_VALUE;
+		temp = new int[m][2]; // 조합으로 뽑힌 2의 위치
 		
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
+			for (int j = 1; j <= n; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 1) home.add(new int[] {i, j});
-				if (map[i][j] == 2) chicken.add(new int[] {i, j});
+				// 1과 2 위치 따로 저장
+				if (map[i][j] == 1) home.add(new int[]{i, j});
+				if (map[i][j] == 2) chicken.add(new int[]{i, j});
 			}
 		}
 		comb(0, 0);
-		
 		System.out.println(ans);
 	}
 
-	private static void comb(int cnt, int start) {
-		if (cnt == M) {
-			int sum = 0;
+	private static void comb(int cnt, int start) { // 치킨집 조합
+		if (cnt == m) {
+			int com = 0;
 			for (int i = 0; i < home.size(); i++) {
-				int min = Integer.MAX_VALUE;
-				for (int j = 0; j < M; j++) {
-					min = Math.min(min, Math.abs(chicken.get(temp[j])[0] - home.get(i)[0]) + Math.abs(chicken.get(temp[j])[1] - home.get(i)[1]));
+				min = Integer.MAX_VALUE;
+				for (int j = 0; j < m; j++) {
+					// chicken과 home의 최소 거리
+					min = Math.min(min, Math.abs(temp[j][0] - home.get(i)[0]) + Math.abs(temp[j][1] - home.get(i)[1]));
 				}
-				sum += min;
+				// chicken과 home의 최소거리의 합 = 치킨 거리
+				com += min;
 			}
-			ans = Math.min(ans, sum);
+			ans = Math.min(com, ans); // 치킨 거리 최소값 구하기
 			return;
 		}
-		
 		for (int i = start; i < chicken.size(); i++) {
-			temp[cnt] = i;
+			temp[cnt] = chicken.get(i);
 			comb(cnt + 1, i + 1);
 		}
 	}
