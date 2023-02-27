8daad79eacf01fc11c647023a219e69e22580861
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-02-27 22:55:16 +0900

    [Silver III] Title: 1로 만들기, Time: 236 ms, Memory: 21668 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] dp = new int[N + 1];
+		
+		for (int i = 2; i < N + 1; i++) {
+			if (i % 6 == 0) {
+				dp[i] = Math.min(dp[i - 1], Math.min(dp[i / 2], dp[i / 3])) + 1;
+			} else if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i - 1], dp[i / 3]) + 1;
+			} else if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i - 1], dp[i / 2]) + 1;
+			}else dp[i] = dp[i - 1] + 1;
+		}
+		
+		System.out.println(dp[N]);
+	}
+}
