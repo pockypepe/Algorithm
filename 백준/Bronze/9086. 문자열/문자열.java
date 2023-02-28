27e4da9dd78c509c8d53c43827206f100c0338d2
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-02-28 17:56:01 +0900

    [Bronze V] Title: 문자열, Time: 228 ms, Memory: 18424 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int i = 0; i < T; i++) {
+			String input = sc.next();
+			System.out.println(input.charAt(0) + "" + input.charAt(input.length() - 1));
+		}
+	}
+}
