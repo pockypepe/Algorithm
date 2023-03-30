2571059f7d00d936f1760585f04fdb94d031bdbe
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-03-30 17:58:31 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 344 ms, Memory: 22336 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,61 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 /*
-    현재 상태, 끝 점과 주변의 벽의 유뮤
+    1. 큐에 시작점과 끝점의 좌표를 배열로 저장
+    2. 파이프의 방향이 어떤 방향이어도 대각선은 항상 체크
+    3. 조건에 따라 가로, 세로 체크
+    4. 만약 끝점이 목표점에 도달하면 1 증가
  */
 public class Main {
     static int N, ans;
     static int[] dx = {1, 0, 1};
     static int[] dy = {0, 1, 1};
     static int[][] map;
-    static Queue<int[]> q;
 
-    private static void pipeMove() {
-        q = new ArrayDeque<>();
-        q.offer(new int[]{0, 0, 0, 1});
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-
-            if (cur[2] == N - 1 && cur[3] == N - 1) {
-                ans++;
-                continue;
-            }
-
-            checkCross(cur);
-            if (cur[0] == cur[2]) { // 가로 상태
-                checkWidth(cur);
-            } else if (cur[1] == cur[3]) { // 세로 상태
-                checkHeight(cur);
-            } else { // 대각선 상태
-                checkWidth(cur);
-                checkHeight(cur);
-            }
-        }
-    }
-
-    private static void checkHeight(int[] cur) {
-        if (isInRange(cur[2] + 1, cur[3]) && map[cur[2] + 1][cur[3]] != 1)
-            q.offer(new int[]{cur[2], cur[3], cur[2] + 1, cur[3]});
-    }
-
-    private static void checkWidth(int[] cur) {
-        if (isInRange(cur[2], cur[3] + 1) && map[cur[2]][cur[3] + 1] != 1)
-            q.offer(new int[]{cur[2], cur[3], cur[2], cur[3] + 1});
-    }
-
-    private static void checkCross(int[] cur) {
-        for (int i = 0; i < 3; i++) {
-            int nx = cur[2] + dx[i];
-            int ny = cur[3] + dy[i];
-            if (isInRange(nx, ny) && map[nx][ny] != 1) continue;
+    private static void pipeMove(int x, int y, int dir) {
+        if (x == N - 1 && y == N - 1) {
+            ans++;
             return;
         }
-        q.offer(new int[]{cur[2], cur[3], cur[2] + 1, cur[3] + 1});
+
+        switch (dir) {
+            case 0 :
+                if (checkGaro(x, y)) pipeMove(x, y + 1, 0);
+                break;
+            case 1 :
+                if (checkSero(x, y)) pipeMove(x + 1, y, 1);
+                break;
+            case 2 :
+                if (checkGaro(x, y)) pipeMove(x, y + 1, 0);
+                if (checkSero(x, y)) pipeMove(x + 1, y, 1);
+                break;
+        }
+
+        if (checkCross(x, y)) pipeMove(x + 1, y + 1, 2);
+    }
+
+    private static boolean checkGaro(int x, int y) {
+        if (isInRange(x, y + 1) && map[x][y + 1] != 1) return true;
+        return false;
+    }
+
+    private static boolean checkSero(int x, int y) {
+        if (isInRange(x + 1, y) && map[x + 1][y] != 1) return true;
+        return false;
+    }
+    private static boolean checkCross(int x, int y) {
+        for (int i = 0; i < 3; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (isInRange(nx, ny) && map[nx][ny] != 1) continue;
+            return false;
+        }
+        return true;
     }
 
     private static boolean isInRange(int nx, int ny) {
@@ -72,7 +67,7 @@ public class Main {
             for (int j = 0; j < N; j++) map[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        pipeMove();
+        pipeMove(0, 1, 0);
 
         System.out.println(ans);
     }
