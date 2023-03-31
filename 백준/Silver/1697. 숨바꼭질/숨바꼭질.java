0fcfcb07481b9f4fce64e1ca86f15a69bb5e92df
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-03-31 10:33:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 184 ms, Memory: 28044 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,48 +1,49 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, K, ans;
-	static boolean[] visited;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		K = sc.nextInt();
-		ans = Integer.MAX_VALUE;
-		visited = new boolean[200001];
-		
-		bfs(N);
-		System.out.println(ans);
-	}
-	private static void bfs(int start) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {start, 0});
-		
-		while (!q.isEmpty()) {
-			int[] current = q.poll();
-			
-			if (visited[current[0]]) continue;
-			visited[current[0]] = true;
+    static int N, K, ans;
+    static boolean[] visited;
 
-			if (current[0] == K) {
-				ans = Math.min(ans, current[1]);
-			}
-			
-			int[] dx = {-1, 1, current[0]};
-			for (int i = 0; i < 3; i++) {
-				int nx = current[0] + dx[i];
-				if (isInRange(nx) && !visited[nx]) {
-					q.offer(new int[] {nx, current[1] + 1});
-				}
-			}
-		}
-		
-	}
-	
-	private static boolean isInRange(int nx) {
-		return 0 <= nx && nx < 200001;
-	}
+    private static void moveSubin() {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{N, 0});
+        visited = new boolean[200001];
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            if (visited[cur[0]]) continue;
+            visited[cur[0]] = true;
+
+            if (cur[0] == K) {
+                ans = cur[1];
+                break;
+            }
+
+            if (cur[0] < 0) continue;
+
+            int[] move = {1, -1, cur[0]};
+            for (int i = 0; i < 3; i++) {
+                int next = cur[0] + move[i];
+                if (isInRange(next) && !visited[next]) q.offer(new int[] {next, cur[1] + 1});
+            }
+        }
+    }
+
+    private static boolean isInRange(int next) {
+        return 0 <= next && next < 200001;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        moveSubin();
+
+        System.out.println(ans);
+    }
 }
