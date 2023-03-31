e90dc5503f7fd3ab3206116c44ce8292ae0add74
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-03-31 23:44:34 +0900

    [Silver I] Title: 쿼드트리, Time: 128 ms, Memory: 14880 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,46 +1,52 @@
 import java.io.*;
 
 public class Main {
-	static int[][] map;
-	static StringBuilder sb;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		
-		for (int i = 0; i < N; i++) {
-			String[] input = br.readLine().split("");
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(input[j]);
-			}
-		}
-		
-		cut(0, 0, N);
-		
-		System.out.println(sb.toString());
-	}
+    static int[][] map;
+    static StringBuilder sb;
 
-	private static void cut(int r, int c, int size) {
-		int sum = 0;
-		
-		for (int i = r, rEnd = r + size; i < rEnd; i++) {
-			for (int j = c, cEnd = c + size; j < cEnd; j++) {
-				sum += map[i][j];
-			}
-		}
-		
-		if (sum == size * size) sb.append("1");
-		else if (sum == 0) sb.append("0");
-		else {
-			int half = size / 2;
-			sb.append("(");
-			cut(r, c, half);
-			cut(r, c + half, half);
-			cut(r + half, c, half);
-			cut(r + half, c + half, half);
-			sb.append(")");
-		}
-	}
+    private static void makeSection(int x, int y, int n) {
+        if (n == 1) {
+            sb.append(map[x][y]);
+            return;
+        }
+
+        int sum = 0;
+        for (int i = x; i < n + x; i++) {
+            for (int j = y; j < n + y; j++) {
+                sum += map[i][j];
+            }
+        }
+
+        if (sum == 0) {
+            sb.append(0);
+            return;
+        }
+        if (sum == n * n) {
+            sb.append(1);
+            return;
+        }
+        int newRange = n / 2;
+        sb.append("(");
+        makeSection(x, y, newRange);
+        makeSection(x, y + newRange, newRange);
+        makeSection(x + newRange, y, newRange);
+        makeSection(x + newRange, y + newRange, newRange);
+        sb.append(")");
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        for (int i = 0; i < N; i ++) {
+            char[] input = br.readLine().toCharArray();
+            for (int j = 0; j < N; j++) map[i][j] = input[j] - '0';
+        }
+
+        makeSection(0, 0, N);
+
+        System.out.println(sb.toString());
+    }
 }
