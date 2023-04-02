a28aebe7ba588faddd339edf670498267bc6093c
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-04-02 22:08:08 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 14760 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,53 +6,55 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, ans;
-	static char[][] map;
-	static boolean[][] visited;
-	static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new char[N][];
-		visited = new boolean[N][M];
-		
-		for(int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
-		
-		bfs(0, 0, 1);
-		
-		System.out.println(ans);
-	}
+    static int N, M, ans;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
+    static char[][] map;
 
-	private static void bfs(int x, int y, int cnt) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {x, y, cnt});
-		
-		while (!q.isEmpty()) {
-			int[] temp = q.poll();
-			
-			if (temp[0] == N - 1 && temp[1] == M - 1) {
-				ans = temp[2];
-				break;
-			}
-			if (visited[temp[0]][temp[1]]) continue;
-			visited[temp[0]][temp[1]] = true;
-			
-			for(int d = 0; d < 4; d++) {
-				int nx = temp[0] + dx[d];
-				int ny = temp[1] + dy[d];
-				if (isInRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == '1') {
-					q.offer(new int[] {nx, ny, temp[2] + 1});
-				}
-			}
-		}
-	}
+    private static void searchPath() {
+        visited = new boolean[N][M];
 
-	private static boolean isInRange(int nx, int ny) {
-		return 0 <= nx && nx < N && 0 <= ny && ny < M;
-	}
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {0, 0, 1});
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            if (cur[0] == N - 1 && cur[1] == M - 1) {
+                ans = cur[2];
+                return;
+            }
+
+            if (visited[cur[0]][cur[1]]) continue;
+            visited[cur[0]][cur[1]] = true;
+
+            for (int d = 0; d < 4; d++) {
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
+
+                if (isInRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == '1')
+                    q.offer(new int[] {nx, ny, cur[2] + 1});
+            }
+        }
+    }
+
+    private static boolean isInRange(int nx, int ny) {
+        return 0 <= nx && nx < N && 0 <= ny && ny < M;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new char[N][M];
+
+        for (int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
+
+        searchPath();
+
+        System.out.println(ans);
+    }
 }
