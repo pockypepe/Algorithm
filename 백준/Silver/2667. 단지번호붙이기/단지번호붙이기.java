cf2f0844ec532d4b9f2229587385eb613b476aa3
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-04-03 17:28:02 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,67 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;
-	static List<Integer> danjis;
-	static String[][] map;
-	static boolean[][] visited;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		map = new String[N][N];
-		visited = new boolean[N][N];
-		danjis = new ArrayList<>();
-		
-		for (int i = 0; i < N; i++) map[i] = br.readLine().split("");
-		
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (map[i][j].equals("1") && !visited[i][j]) {
-					cnt++;
-					bfs(new int[] {i, j});
-				}
-			}
-		}
-		
-		Collections.sort(danjis);
-		
-		System.out.println(cnt);
-		for (int i : danjis) {
-			if(i == 0) {
-				System.out.println(i + 1);
-				continue;
-			}
-			System.out.println(i);
-		}
-	}
-	
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	
-	private static void bfs(int[] start) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(start);
-		
-		int danji = 0;
-		while (!q.isEmpty()) {
-			int[] temp = q.poll();
-			if (visited[temp[0]][temp[1]]) continue;
-            visited[temp[0]][temp[1]] = true;
-            danji++;
-			for (int i = 0; i < 4; i++) {
-				int nx = temp[0] + dx[i];
-				int ny = temp[1] + dy[i];
-				if (isInRange(nx, ny) && map[nx][ny].equals("1") && !visited[nx][ny]) {
-					q.offer(new int[] {nx, ny});
-				}
-			}
-		}
-		danjis.add(danji);
-	}
+    static int N;
+    static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+    static boolean[][] visited;
+    static char[][] map;
 
-	private static boolean isInRange(int nx, int ny) {
-		return 0 <= nx && nx < N && 0 <= ny && ny < N;
-	}
+    private static int countDanji(int x, int y) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {x, y});
+
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            if (visited[cur[0]][cur[1]]) continue;
+            visited[cur[0]][cur[1]] = true;
+            cnt++;
+            for (int d = 0; d < 4; d++) {
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
+                if (isInRange(nx, ny) && map[nx][ny] == '1' && !visited[nx][ny]) {
+                    q.offer(new int[] {nx, ny});
+                }
+            }
+        }
+        return cnt;
+    }
+
+    private static boolean isInRange(int nx, int ny) {
+        return 0 <= nx && nx < N && 0 <= ny && ny < N;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        for (int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
+
+        List<Integer> danji = new ArrayList<>();
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == '1' && !visited[i][j]) {
+                    danji.add(countDanji(i, j));
+                }
+            }
+        }
+        Collections.sort(danji);
+        System.out.println(danji.size());
+        for (int i : danji) System.out.println(i);
+    }
 }
