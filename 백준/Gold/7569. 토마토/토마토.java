9e781c92d2191dffbf5be9f2e68e51b4e3877f72
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-04-09 14:18:26 +0900

    [Gold V] Title: 토마토, Time: 784 ms, Memory: 157508 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,78 +1,78 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int M, N, H, ans;
-    static int[] dx = {-1, 1, 0, 0, 0, 0}, dy = {0, 0, -1, 1, 0, 0}, dz = {0, 0, 0, 0, -1, 1};
+    static int N, M, H, zeroCnt, ans;
+    static int[] dx = {-1, 0, 1, 0, 0, 0};
+    static int[] dy = {0, 1, 0, -1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, -1, 1};
     static int[][][] map;
     static boolean[][][] visited;
-    static List<int[]> tomato;
+    static List<int[]> list;
+
+    private static void changeTomato() {
+        visited = new boolean[H][N][M];
+        Queue<int[]> q = new ArrayDeque<>();
+        for (int[] tomato : list) q.offer(tomato);
+
+        int time = 0;
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            if (visited[cur[2]][cur[0]][cur[1]]) continue;
+            visited[cur[2]][cur[0]][cur[1]] = true;
+            time = Math.max(time, cur[3]);
+            if (map[cur[2]][cur[0]][cur[1]] == 0) zeroCnt--;
+
+            if (zeroCnt == 0) {
+                ans = time;
+                break;
+            }
+
+            for (int d = 0; d < 6; d++) {
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
+                int nz = cur[2] + dz[d];
+
+                if (isInRange(nx, ny, nz) && !visited[nz][nx][ny] && map[nz][nx][ny] == 0) {
+                    q.offer(new int[] {nx, ny, nz, cur[3] + 1});
+                }
+            }
+        }
+
+        if (zeroCnt != 0) ans = -1;
+    }
+
+    private static boolean isInRange(int nx, int ny, int nz) {
+        return 0 <= nx && nx < N && 0 <= ny && ny < M && 0 <= nz && nz < H;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        M = Integer.parseInt(st.nextToken()); // y
-        N = Integer.parseInt(st.nextToken()); // x
-        H = Integer.parseInt(st.nextToken()); // z
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-        map = new int[H][N][M]; // z x y
-        visited = new boolean[H][N][M];
-        tomato = new ArrayList<>();
-
-        for (int i = 0; i < H; i++){
-            for (int j = 0; j < N; j++){
+        map = new int[H][N][M];
+        list = new ArrayList<>();
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
                 st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < M; k++){
-                    map[i][j][k] = Integer.parseInt(st.nextToken());
-                    if (map[i][j][k] == 1) tomato.add(new int[] {i, j, k, 0}); // z x y level
+                for (int k = 0; k < M; k++) {
+                    int temp = Integer.parseInt(st.nextToken());
+                    map[i][j][k] = temp;
+                    if (temp == 1) list.add(new int[] {j, k, i, 0}); // x, y, z, time
+                    if (temp == 0) zeroCnt++;
                 }
             }
         }
 
-        mature();
+        changeTomato();
 
         System.out.println(ans);
-    }
-
-    private static void mature() {
-        Queue<int[]> queue = new ArrayDeque<>();
-        for (int[] i : tomato) queue.offer(i);
-        
-        int days = 0;
-        while(!queue.isEmpty()){
-            int[] current = queue.poll();
-
-            if (visited[current[0]][current[1]][current[2]]) continue;
-            visited[current[0]][current[1]][current[2]] = true;
-            days = Math.max(days, current[3]);
-            
-            for (int d = 0; d < 6; d++){
-                int nx = current[1] + dx[d];
-                int ny = current[2] + dy[d];
-                int nz = current[0] + dz[d];
-                if (isInRange(nx, ny, nz) && !visited[nz][nx][ny] && map[nz][nx][ny] != -1){
-                	map[nz][nx][ny] = 1;
-                    queue.offer(new int[]{nz, nx, ny, current[3] + 1});
-                }
-            }
-        }
-        
-        int cnt = 0;
-        for (int i = 0; i < H; i++){
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < M; k++) {
-                    if (map[i][j][k] == 0) cnt++;
-                }
-            }
-        }
-
-        if (cnt == 0) ans = days;
-        else ans = -1;
-
-    }
-
-    private static boolean isInRange(int nx, int ny, int nz){
-        return 0 <= nx && nx < N && 0 <= ny && ny < M && 0 <= nz && nz < H;
     }
 }
