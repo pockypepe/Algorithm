327801ef31a498593df5424b357a053f588fff9f
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-04-11 22:26:22 +0900

    [Gold V] Title: 토마토, Time: 688 ms, Memory: 128840 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,72 +1,77 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int M, N, ans;
-    static int[] dx = {-1, 1, 0, 0}, dy = {0, 0, -1, 1};
+    static int N, M, ans;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int[][] map;
-    static boolean[][] visited;
-    static List<int[]> tomato;
+    static List<Tomato> tomato;
+
+    static class Tomato {
+        int x, y;
+
+        public Tomato(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    private static void mature() {
+        Queue<int[]> q = new ArrayDeque<>();
+        for (Tomato t : tomato) q.offer(new int[] {t.x, t.y, 0});
+
+        int time = 0;
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            time = Math.max(time, cur[2]);
+
+            for (int d = 0; d < 4; d++) {
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
+
+                if (isInRange(nx, ny)  && map[nx][ny] == 0) {
+                    q.offer(new int[] {nx, ny, cur[2] + 1});
+                    map[nx][ny] = 1;
+                }
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 0) cnt++;
+            }
+        }
+
+        if (cnt == 0) ans = time;
+        else ans = -1;
+    }
+
+    private static boolean isInRange(int nx, int ny) {
+        return 0 <= nx && nx < M && 0 <= ny && ny < N;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-
-        map = new int[N][M];
-        visited = new boolean[N][M];
+        M = Integer.parseInt(st.nextToken());
+        map = new int[M][N];
         tomato = new ArrayList<>();
 
-        for (int i = 0; i < N; i++){
+        for (int i = 0 ; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 1) tomato.add(new int[] {i, j, 0});
+            for (int j = 0; j < N; j++) {
+                int temp = Integer.parseInt(st.nextToken());
+                map[i][j] = temp;
+                if (temp == 1) tomato.add(new Tomato(i, j));
             }
         }
 
         mature();
 
         System.out.println(ans);
-    }
-
-    private static void mature() {
-        Queue<int[]> queue = new ArrayDeque<>();
-        for (int[] i : tomato) queue.offer(i);
-        
-        int days = 0;
-        while(!queue.isEmpty()){
-            int[] current = queue.poll();
-
-            if (visited[current[0]][current[1]]) continue;
-            visited[current[0]][current[1]] = true;
-            days = Math.max(days, current[2]);
-            
-            for (int d = 0; d < 4; d++){
-                int nx = current[0] + dx[d];
-                int ny = current[1] + dy[d];
-                if (isInRange(nx, ny) && !visited[nx][ny] && map[nx][ny] != -1){
-                	map[nx][ny] = 1;
-                    queue.offer(new int[]{nx, ny, current[2] + 1});
-                }
-            }
-        }
-        
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-        	for (int j = 0; j < M; j++) {
-        		if (map[i][j] == 0) cnt++;
-        	}
-        }
-
-        if (cnt == 0) ans = days;
-        else ans = -1;
-
-    }
-
-    private static boolean isInRange(int nx, int ny){
-        return 0 <= nx && nx < N && 0 <= ny && ny < M;
     }
 }
