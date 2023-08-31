4bcc39427a85a991b52d38c6a90f7867ec54e25a
pockypepe <94898193+pockypepe@users.noreply.github.com>
2023-08-31 19:57:33 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,60 +1,60 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, ans;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static boolean[][] visited;
+    static int N, M;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static char[][] map;
+    static boolean[][] visited;
 
-    private static void searchPath() {
-        visited = new boolean[N][M];
-
+    private static int bfs() {
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[] {0, 0, 1});
+        int ans = 0;
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
 
-            if (cur[0] == N - 1 && cur[1] == M - 1) {
-                ans = cur[2];
-                return;
-            }
-
             if (visited[cur[0]][cur[1]]) continue;
             visited[cur[0]][cur[1]] = true;
+
+            if (cur[0] == (N - 1) && cur[1] == (M - 1)) {
+                ans = cur[2];
+                break;
+            }
 
             for (int d = 0; d < 4; d++) {
                 int nx = cur[0] + dx[d];
                 int ny = cur[1] + dy[d];
 
-                if (isInRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == '1')
+                if (isInRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == '1') {
                     q.offer(new int[] {nx, ny, cur[2] + 1});
+                }
             }
         }
+
+        return ans;
     }
 
     private static boolean isInRange(int nx, int ny) {
         return 0 <= nx && nx < N && 0 <= ny && ny < M;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         map = new char[N][M];
+        visited = new boolean[N][M];
 
         for (int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
 
-        searchPath();
-
-        System.out.println(ans);
+        System.out.println(bfs());
     }
 }
